99ef1700bf4f42e9c232fc143e1fecd7c6bfe50f
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 15:08:48 +0300

    Update private.py

--- source/YukkiMusic/plugins/sudo/private.py
+++ source/YukkiMusic/plugins/sudo/private.py
@@ -13,13 +13,13 @@ from pyrogram.types import Message
 
 import config
 from strings import get_command
-from YukkiMusic import app
+from AlexaMusic import app
 from config.config import OWNER_ID
-from YukkiMusic.utils.database import (add_private_chat,
+from AlexaMusic.utils.database import (add_private_chat,
                                        get_private_served_chats,
                                        is_served_private_chat,
                                        remove_private_chat)
-from YukkiMusic.utils.decorators.language import language
+from AlexaMusic.utils.decorators.language import language
 
 AUTHORIZE_COMMAND = get_command("AUTHORIZE_COMMAND")
 UNAUTHORIZE_COMMAND = get_command("UNAUTHORIZE_COMMAND")
