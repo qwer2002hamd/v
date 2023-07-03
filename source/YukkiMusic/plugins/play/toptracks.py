c6b5bf39fa1df943f66c93140bda071ff1894ec0
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 14:59:14 +0300

    Update toptracks.py

--- source/YukkiMusic/plugins/play/toptracks.py
+++ source/YukkiMusic/plugins/play/toptracks.py
@@ -13,14 +13,14 @@ from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup
 
 from config import BANNED_USERS
-from YukkiMusic import app
-from YukkiMusic.utils.database import (get_global_tops,
+from AlexaMusic import app
+from AlexaMusic.utils.database import (get_global_tops,
                                        get_particulars, get_userss)
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.inline.playlist import (botplaylist_markup,
+from AlexaMusic.utils.decorators.language import languageCB
+from AlexaMusic.utils.inline.playlist import (botplaylist_markup,
                                               failed_top_markup,
                                               top_play_markup)
-from YukkiMusic.utils.stream.stream import stream
+from AlexaMusic.utils.stream.stream import stream
 
 loop = asyncio.get_running_loop()
 
