01939c57772c2eb0dbf2e5fee02abc90d1391b3c
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 15:08:13 +0300

    Update maintenance.py

--- source/YukkiMusic/plugins/sudo/maintenance.py
+++ source/YukkiMusic/plugins/sudo/maintenance.py
@@ -12,12 +12,12 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command, get_string
-from YukkiMusic import app
+from AlexaMusic import app
 from config.config import OWNER_ID
-from YukkiMusic.utils.database import (get_lang, is_maintenance,
+from AlexaMusic.utils.database import (get_lang, is_maintenance,
                                        maintenance_off,
                                        maintenance_on)
-from YukkiMusic.utils.decorators.language import language
+from AlexaMusic.utils.decorators.language import language
 
 # Commands
 MAINTENANCE_COMMAND = get_command("MAINTENANCE_COMMAND")
