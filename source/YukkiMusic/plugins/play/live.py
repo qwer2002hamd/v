38d8ed93d141dc600884698a9b4043dd4310d726
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 14:48:25 +0300

    Update live.py

--- source/YukkiMusic/plugins/play/live.py
+++ source/YukkiMusic/plugins/play/live.py
@@ -10,10 +10,10 @@
 from pyrogram import filters
 
 from config import BANNED_USERS
-from YukkiMusic import YouTube, app
-from YukkiMusic.utils.channelplay import get_channeplayCB
-from YukkiMusic.utils.decorators.language import languageCB
-from YukkiMusic.utils.stream.stream import stream
+from AlexaMusic import YouTube, app
+from AlexaMusic.utils.channelplay import get_channeplayCB
+from AlexaMusic.utils.decorators.language import languageCB
+from AlexaMusic import stream
 
 
 @app.on_callback_query(filters.regex("LiveStream") & ~BANNED_USERS)
