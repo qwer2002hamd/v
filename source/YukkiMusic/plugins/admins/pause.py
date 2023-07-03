54a24f64b6ad69b7f7ee50bed966d40a81878b0d
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 14:18:54 +0300

    Update pause.py

--- source/YukkiMusic/plugins/admins/pause.py
+++ source/YukkiMusic/plugins/admins/pause.py
@@ -7,16 +7,16 @@
 #
 # All rights reserved.
 
-from YukkiMusic.plugins.play.filters import command
+from AlexaMusic.plugins.play.filters import command
 from pyrogram import filters
 from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import is_music_playing, music_off
-from YukkiMusic.utils.decorators import AdminRightsCheckCB
+from AlexaMusic import app
+from AlexaMusic.core.call import Alexa
+from AlexaMusic.utils.database import is_music_playing, music_off
+from AlexaMusic.utils.decorators import AdminRightsCheckCB
 
 # Commands
 PAUSE_COMMAND = get_command("PAUSE_COMMAND")
@@ -34,7 +34,7 @@ async def pause_admin(cli, message: Message, _, chat_id):
     if not await is_music_playing(chat_id):
         return await message.reply_text(_["admin_1"])
     await music_off(chat_id)
-    await Yukki.pause_stream(chat_id)
+    await Alexa.pause_stream(chat_id)
     if message.sender_chat:
         mention = f'<a href=tg://user?id={message.chat.id}>{message.chat.title}</a>'
     else:
