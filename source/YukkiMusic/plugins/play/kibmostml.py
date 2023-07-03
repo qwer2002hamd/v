7f91523499ec57e3ee3e537fa1aa17a211baacdf
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 14:47:02 +0300

    Update kibmostml.py

--- source/YukkiMusic/plugins/play/kibmostml.py
+++ source/YukkiMusic/plugins/play/kibmostml.py
@@ -12,8 +12,8 @@ import random
 from pyrogram import Client
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from strings.filters import command
-from YukkiMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
-from YukkiMusic import app
+from AlexaMusic import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
+from AlexaMusic import app
 from random import  choice, randint
 
 
