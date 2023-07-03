77ae867e66fe8aec3b279f60127c81dcd0fc522e
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 14:31:55 +0300

    Update autoleave.py

--- source/YukkiMusic/plugins/misc/autoleave.py
+++ source/YukkiMusic/plugins/misc/autoleave.py
@@ -11,9 +11,9 @@ import asyncio
 from datetime import datetime
 
 import config
-from YukkiMusic import app
-from YukkiMusic.core.call import Yukki, autoend
-from YukkiMusic.utils.database import (get_client, is_active_chat,
+from AlexaMusic import app
+from AlexaMusic.core.call import Alexa, autoend
+from AlexaMusic.utils.database import (get_client, is_active_chat,
                                        is_autoend)
 
 
@@ -22,7 +22,7 @@ async def auto_leave():
         while not await asyncio.sleep(
             config.AUTO_LEAVE_ASSISTANT_TIME
         ):
-            from YukkiMusic.core.userbot import assistants
+            from AlexaMusic.core.userbot import assistants
 
             for num in assistants:
                 client = await get_client(num)
@@ -37,9 +37,7 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001190342892
-                                and chat_id != -1001733534088
-                                and chat_id != -1001443281821
+                                and chat_id != -1001420714100
                             ):
                                 if not await is_active_chat(chat_id):
                                     try:
@@ -69,13 +67,13 @@ async def auto_end():
                     continue
                 autoend[chat_id] = {}
                 try:
-                    await Yukki.stop_stream(chat_id)
+                    await Alexa.stop_stream(chat_id)
                 except:
                     continue
                 try:
                     await app.send_message(
                         chat_id,
-                        "#AFYONA\n\n🥹♥ خرج المساعد من المحادثه الصوتيه لعدم وجود شخص في المحادثه\n√",
+                        "🥹♥ خرج المساعد من المحادثه الصوتيه لعدم وجود شخص في المحادثه\n√",
                     )
                 except:
                     continue
