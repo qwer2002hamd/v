6a603511efcc5c36823abb6193b001a32823ba69
qwer2002hamd <136924700+qwer2002hamd@users.noreply.github.com>
2023-07-03 15:12:37 +0300

    Update active.py

--- source/YukkiMusic/plugins/tools/active.py
+++ source/YukkiMusic/plugins/tools/active.py
@@ -12,9 +12,9 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from strings import get_command
-from YukkiMusic import app
-from YukkiMusic.misc import SUDOERS
-from YukkiMusic.utils.database.memorydatabase import (
+from AlexaMusic import app
+from AlexaMusic.misc import SUDOERS
+from AlexaMusic.utils.database.memorydatabase import (
     get_active_chats, get_active_video_chats)
 
 # Commands
